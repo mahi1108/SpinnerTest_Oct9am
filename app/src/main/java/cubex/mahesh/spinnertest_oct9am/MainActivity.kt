138d2707d296c2582cc924732d049cbc177d2dac
maheshthippala <mahesh.cubex@gmail.com>
2018-10-06 11:07:43 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/spinnertest_oct9am/MainActivity.kt
@@ -0,0 +1,31 @@
+package cubex.mahesh.spinnertest_oct9am
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sp1.onItemSelectedListener =
+                          object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if(p2>0) {
+                    Toast.makeText(this@MainActivity,
+                            sp1.selectedItem.toString(),
+                            Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+    }
+}
